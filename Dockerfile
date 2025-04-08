dd3adcc5e37115f83b49158d9a3fd087cc0c9ed3
ashutosh61 <uikeyashutosh3@gmail.com>
2025-04-08 05:59:43 -0600

    updatted

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17
-COPY target/HelloApi.war  /user/app/
-WORKDIR /user/app/
+COPY target/HelloApi.war  /usr/app/
+WORKDIR /usr/app/
 ENTRYPOINT ["java","-jar","HelloApi.war"]
--- src/main/java/com/nt/Controller/hello.java
+++ src/main/java/com/nt/Controller/hello.java
@@ -1,5 +1,13 @@
 package com.nt.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hello {
 
+	@GetMapping("/get")
+	public String helloApi() {
+		return "hello paaji";
+	}
 }
